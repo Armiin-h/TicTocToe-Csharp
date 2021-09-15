7d4ad8cf17f088c7f0326650104e253f96e3d3ab
Armin Heidari <90096157+Armin-Heidari@users.noreply.github.com>
2021-09-15 17:37:01 +0430

    Add files via upload

---
+++ AI.cs
@@ -0,0 +1,370 @@
+﻿using System;
+
+namespace TicTacToeAI
+{
+	public static class AI
+	{
+		public static bool wentFirst = false;
+		public static bool moveSuccesful = false;
+
+		public static int move = 0;
+		public static int forkNum = 0;
+		public static int thirdForkNum = 0;
+
+        public static void AITurn()
+        {
+			Console.Clear();
+            Console.WriteLine("It is the computers turn.");
+            Program.PrintGameBoard();
+
+            StrategyLoop();
+			
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+			Program.CheckWinConditions();
+            Program.PlayerTurn();
+        }
+
+        public static void StrategyLoop()
+        {
+			moveSuccesful = false;
+
+            Win();
+			if (moveSuccesful) { move++; return; }
+
+            Block();
+			if (moveSuccesful) { move++; return; }
+
+			Fork();
+			if (moveSuccesful) { move++; return; }
+
+			BlockFork();
+			if (moveSuccesful) {move++; return; }
+
+			PlayCenter();
+			if (moveSuccesful) { move++; return; }
+
+			PlayOppositeCorner();
+			if (moveSuccesful) { move++; return; }
+
+			PlayEmptyCorner();
+			if (moveSuccesful) { move++; return; }
+
+			PlayEmptySide();
+			if (moveSuccesful) { move++; return; }
+
+		}
+
+        public static void PlayEmptySide()
+        {
+			int[] b = Program.gameBoard;
+
+			if (b[1] == 0) { b[1] = 2; moveSuccesful = true; return; }//top middle
+
+			if (b[3] == 0) { b[3] = 2; moveSuccesful = true; return; }//middle left
+
+			if (b[5] == 0) { b[5] = 2; moveSuccesful = true; return; }//middle right
+
+			if (b[7] == 0) { b[7] = 2; moveSuccesful = true; return; }//bottom middle
+		}
+
+        public static void PlayEmptyCorner()
+        {
+			int[] b = Program.gameBoard;
+
+            if (b[0] == 0) { b[0] = 2; moveSuccesful = true; return; }//top left
+
+			if (b[2] == 0) { b[2] = 2; moveSuccesful = true; return; }//top right
+
+			if (b[6] == 0) { b[6] = 2; moveSuccesful = true; return; }//bottom left
+
+			if (b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }//bottom right
+		}
+
+        public static void PlayOppositeCorner()
+        {
+			int[] b = Program.gameBoard;
+
+			//top left, bottom right
+            if (b[0] == 1 && b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }
+			if (b[8] == 1 && b[0] == 0) { b[0] = 2; moveSuccesful = true; return; }
+
+			//top right, bottom left
+			if (b[2] == 1 && b[6] == 0) { b[6] = 2; moveSuccesful = true; return; }
+			if (b[6] == 1 && b[2] == 6) { b[8] = 2; moveSuccesful = true; return; }
+		}
+
+        public static void PlayCenter()
+        {
+			int[] b = Program.gameBoard;
+
+            if(b [4] == 0) { b[4] = 2; moveSuccesful = true; return; }
+        }
+
+        public static void BlockFork()
+        {
+			int[] b = Program.gameBoard;
+
+            if(move == 1)
+			{
+				if(b[0] == 1 && b[8] == 1 && b[4] == 2)
+				{
+					PlayEmptySide();
+				}
+
+				if (b[2] == 1 && b[6] == 1 && b[4] == 2)
+				{
+					PlayEmptySide();
+				}
+			}
+        }
+
+        public static void Fork()
+        {
+			int[] b = Program.gameBoard;
+			Random random = new Random();
+
+			if (wentFirst)
+			{
+				if (move == 0)
+				{
+					forkNum = random.Next(0, 4);
+
+					if (forkNum == 0 && b[0] == 0) //top left first move
+					{
+						b[0] = 2;
+						moveSuccesful = true;
+						return;
+					}
+
+					if (forkNum == 1 && b[2] == 0) //top right first move
+					{
+						b[2] = 2;
+						moveSuccesful = true;
+						return;
+					}
+
+					if (forkNum == 2 && b[6] == 0)//bottom right first move
+					{
+						b[6] = 2;
+						moveSuccesful = true;
+						return;
+					}
+
+					if (forkNum == 3 && b[8] == 0)
+					{
+						b[8] = 2;
+						moveSuccesful = true;
+						return;
+					}
+				}
+
+				if (move == 1)
+				{
+					#region 1st option
+					if (forkNum == 0 && b[8] == 0) //bottom right second move
+					{
+						b[8] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if(forkNum == 0 && b[6] == 0)
+					{
+						b[6] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if(forkNum == 0 && b[2] == 0)
+					{
+						b[2] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					#endregion
+
+					#region 2nd option
+					if (forkNum == 1 && b[6] == 0) //bottom left second move
+					{
+						b[6] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if (forkNum == 0 && b[0] == 0)
+					{
+						b[0] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if (forkNum == 0 && b[8] == 0)
+					{
+						b[8] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					#endregion
+
+					#region 3rd option
+					if (forkNum == 2 && b[2] == 0)//top right second move
+					{
+						b[2] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if (forkNum == 0 && b[0] == 0)
+					{
+						b[0] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if (forkNum == 0 && b[8] == 0)
+					{
+						b[8] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					#endregion
+
+					#region 4th option
+					if (forkNum == 3 && b[0] == 0)//top left second move
+					{
+						b[0] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if (forkNum == 0 && b[6] == 0)
+					{
+						b[6] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					else if (forkNum == 0 && b[2] == 0)
+					{
+						b[2] = 2;
+						moveSuccesful = true;
+						return;
+					}
+					#endregion
+				}
+
+				if (move == 2)
+				{
+					thirdForkNum = random.Next(0, 2);
+
+					if (forkNum == 0 && thirdForkNum == 0 && b[2] == 0 || forkNum == 2 && thirdForkNum == 0 && b[2] == 0)// 1/2 first fork option
+					{
+						b[2] = 2;
+						moveSuccesful = true;
+						return;
+					}
+
+					if (forkNum == 0 && thirdForkNum == 1 && b[6] == 0 || forkNum == 2 && thirdForkNum == 1 && b[6] == 0)// 2/2 second fork option
+					{
+						b[6] = 2;
+						moveSuccesful = true;
+						return;
+					}
+
+					if (forkNum == 1 && thirdForkNum == 0 && b[0] == 0 || forkNum == 3 && thirdForkNum == 0 && b[0] == 0)// 1/2 first fork option
+					{
+						b[0] = 2;
+						moveSuccesful = true;
+						return;
+					}
+
+					if (forkNum == 1 && thirdForkNum == 1 && b[8] == 0 || forkNum == 3 && thirdForkNum == 1 && b[8] == 0)// 2/2 second fork option
+					{
+						b[8] = 2;
+						moveSuccesful = true;
+						return;
+					}
+				}
+			}
+        }
+
+        public static void Block()
+        {
+			int[] b = Program.gameBoard;
+
+			//Start of across
+			if (b[0] == 0 && b[1] == 1 && b[2] == 1) { b[0] = 2; moveSuccesful = true; return; }
+			if (b[0] == 1 && b[1] == 0 && b[2] == 1) { b[1] = 2; moveSuccesful = true; return; }
+			if (b[0] == 1 && b[1] == 1 && b[2] == 0) { b[2] = 2; moveSuccesful = true; return; }
+
+			if (b[3] == 0 && b[4] == 1 && b[5] == 1) { b[3] = 2; moveSuccesful = true; return; }
+			if (b[3] == 1 && b[4] == 0 && b[5] == 1) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[3] == 1 && b[4] == 1 && b[5] == 0) { b[5] = 2; moveSuccesful = true; return; }
+
+			if (b[6] == 0 && b[7] == 1 && b[8] == 1) { b[6] = 2; moveSuccesful = true; return; }
+			if (b[6] == 1 && b[7] == 0 && b[8] == 1) { b[7] = 2; moveSuccesful = true; return; }
+			if (b[6] == 1 && b[7] == 1 && b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }
+			//end of across
+
+			//Start of vertical
+			if (b[0] == 0 && b[3] == 1 && b[6] == 1) { b[0] = 2; moveSuccesful = true; return; }
+			if (b[0] == 1 && b[3] == 0 && b[6] == 1) { b[3] = 2; moveSuccesful = true; return; }
+			if (b[0] == 1 && b[3] == 1 && b[6] == 0) { b[6] = 2; moveSuccesful = true; return; }
+
+			if (b[1] == 0 && b[4] == 1 && b[7] == 1) { b[1] = 2; moveSuccesful = true; return; }
+			if (b[1] == 1 && b[4] == 0 && b[7] == 1) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[1] == 1 && b[4] == 1 && b[7] == 0) { b[7] = 2; moveSuccesful = true; return; }
+
+			if (b[2] == 0 && b[5] == 1 && b[8] == 1) { b[2] = 2; moveSuccesful = true; return; }
+			if (b[2] == 1 && b[5] == 0 && b[8] == 1) { b[5] = 2; moveSuccesful = true; return; }
+			if (b[2] == 1 && b[5] == 1 && b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }
+			//end of vertical
+
+			//start of diagonal
+			if (b[0] == 0 && b[4] == 1 && b[8] == 1) { b[0] = 2; moveSuccesful = true; return; }
+			if (b[0] == 1 && b[4] == 0 && b[8] == 1) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[0] == 1 && b[4] == 1 && b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }
+
+			if (b[2] == 0 && b[4] == 1 && b[6] == 1) { b[2] = 2; moveSuccesful = true; return; }
+			if (b[2] == 1 && b[4] == 0 && b[6] == 1) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[2] == 1 && b[4] == 1 && b[6] == 0) { b[6] = 2; moveSuccesful = true; return; }//end of diagonal
+		}
+
+        public static void Win()
+        {
+			int[] b = Program.gameBoard;
+
+			//Start of across
+			if (b[0] == 0 && b[1] == 2 && b[2] == 2) { b[0] = 2; moveSuccesful = true; return; }
+			if (b[0] == 2 && b[1] == 0 && b[2] == 2) { b[1] = 2; moveSuccesful = true; return; }
+			if (b[0] == 2 && b[1] == 2 && b[2] == 0) { b[2] = 2; moveSuccesful = true; return; }
+
+			if (b[3] == 0 && b[4] == 2 && b[5] == 2) { b[3] = 2; moveSuccesful = true; return; }
+			if (b[3] == 2 && b[4] == 0 && b[5] == 2) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[3] == 2 && b[4] == 2 && b[5] == 0) { b[5] = 2; moveSuccesful = true; return; }
+
+			if (b[6] == 0 && b[7] == 2 && b[8] == 2) { b[6] = 2; moveSuccesful = true; return; }
+			if (b[6] == 2 && b[7] == 0 && b[8] == 2) { b[7] = 2; moveSuccesful = true; return; }
+			if (b[6] == 2 && b[7] == 2 && b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }
+			//end of across
+
+			//Start of vertical
+			if (b[0] == 0 && b[3] == 2 && b[6] == 2) { b[0] = 2; moveSuccesful = true; return; }
+			if (b[0] == 2 && b[3] == 0 && b[6] == 2) { b[3] = 2; moveSuccesful = true; return; }
+			if (b[0] == 2 && b[3] == 2 && b[6] == 0) { b[6] = 2; moveSuccesful = true; return; }
+
+			if (b[1] == 0 && b[4] == 2 && b[7] == 2) { b[1] = 2; moveSuccesful = true; return; }
+			if (b[1] == 2 && b[4] == 0 && b[7] == 2) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[1] == 2 && b[4] == 2 && b[7] == 0) { b[7] = 2; moveSuccesful = true; return; }
+
+			if (b[2] == 0 && b[5] == 2 && b[8] == 2) { b[2] = 2; moveSuccesful = true; return; }
+			if (b[2] == 2 && b[5] == 0 && b[8] == 2) { b[5] = 2; moveSuccesful = true; return; }
+			if (b[2] == 2 && b[5] == 2 && b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }
+			//end of vertical
+
+			//start of diagonal
+			if (b[0] == 0 && b[4] == 2 && b[8] == 2) { b[0] = 2; moveSuccesful = true; return; }
+			if (b[0] == 2 && b[4] == 0 && b[8] == 2) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[0] == 2 && b[4] == 2 && b[8] == 0) { b[8] = 2; moveSuccesful = true; return; }
+
+			if (b[2] == 0 && b[4] == 2 && b[6] == 2) { b[2] = 2; moveSuccesful = true; return; }
+			if (b[2] == 2 && b[4] == 0 && b[6] == 2) { b[4] = 2; moveSuccesful = true; return; }
+			if (b[2] == 2 && b[4] == 2 && b[6] == 0) { b[6] = 2; moveSuccesful = true; return; }
+			//end of diagonal
+		}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+
+namespace TicTacToeAI
+{
+    class Program
+    {
+		public static int iterations = 0;
+		public static int turn = 0;
+
+		public static int[] gameBoard = new int[9];
+
+		public static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+			Console.Clear();
+
+			if(iterations == 0)
+			{
+				Console.WriteLine("Heads or Tails?");
+				string choice = Console.ReadLine().ToLower();
+
+				Start(choice);
+			}
+			iterations++;
+			Console.Clear();
+
+			switch (turn)
+			{
+				case 0:
+					PlayerTurn();
+					break;
+				case 1:
+					AI.AITurn();
+					break;
+			}
+
+        }
+
+		public static void Start(string choice)
+		{
+			int coinFlip = random.Next(1, 3);
+
+			switch (choice)
+			{
+				case "heads":
+					
+					if (coinFlip == 1)
+					{
+						Console.WriteLine("You won the coinflip so you go first.  You are Xs.  Press any key to continue.");
+						turn = 0;
+						Console.ReadKey();
+					}
+					else
+					{
+						Console.WriteLine("You lost the coinflip so you go second.  You are Xs.  Press any key to continue.");
+						AI.wentFirst = true;
+						turn = 1;
+						Console.ReadKey();
+					}
+					break;
+				case "tails":
+					if (coinFlip == 2)
+					{
+						Console.WriteLine("You won the coinflip so you go first.  You are Xs.  Press any key to continue.");
+						turn = 0;
+						Console.ReadKey();
+					}
+					else
+					{
+						Console.WriteLine("You lost the coinflip so you go second.  You are Xs.  Press any key to continue.");
+						AI.wentFirst = true;
+						turn = 1;
+						Console.ReadKey();
+					}
+					break;
+				default:
+					Console.WriteLine("Please choose heads or tails.  Press any button to continue.");
+					Console.ReadKey();
+					Main(new string[0]);
+					break;
+			}
+		}
+
+		public static void PlayerTurn()
+		{
+			Console.Clear();
+			Console.WriteLine("Its your turn.");
+
+			PrintGameBoard();
+			Console.WriteLine("Which slot would you like to place an x?");
+
+			string choice = Console.ReadLine();
+			int a;
+			int.TryParse(choice, out a);
+			
+			if(a <= 0 || a > 9)
+			{
+				Console.WriteLine("You did not choose a valid place.  Press any key to try again.");
+				Console.ReadKey();
+				PlayerTurn();
+			}
+			
+			if(gameBoard[int.Parse(choice) - 1] == 0)
+				gameBoard[int.Parse(choice)-1] = 1;
+			else
+			{
+				Console.WriteLine("You did not choose a valid place.  Press any key to try again.");
+				Console.ReadKey();
+				PlayerTurn();
+			}
+			
+			CheckWinConditions();
+			AI.AITurn();
+		}
+
+		public static void PrintGameBoard()
+		{
+			string VisualGameBoard = "";
+
+			for (int i = 0; i < gameBoard.Length; i++)
+			{
+				if (i == 3 || i == 6)
+					VisualGameBoard += "\r\n";
+
+				switch (gameBoard[i])
+				{
+					case 0:
+						VisualGameBoard += " _";
+						break;
+					case 1:
+						VisualGameBoard += " X";
+						break;
+					case 2:
+						VisualGameBoard += " O";
+						break;
+
+				}
+			}
+			Console.WriteLine(VisualGameBoard);
+		}
+
+		public static void CheckWinConditions()
+		{
+			int[] b = gameBoard;
+			if (b[0] == 1 && b[1] == 1 && b[2] == 1) { Win(1); }//Start of x win conditions
+			if (b[3] == 1 && b[4] == 1 && b[5] == 1) { Win(1); }
+			if (b[6] == 1 && b[7] == 1 && b[8] == 1) { Win(1); }
+			if (b[0] == 1 && b[3] == 1 && b[6] == 1) { Win(1); }
+			if (b[1] == 1 && b[4] == 1 && b[7] == 1) { Win(1); }
+			if (b[2] == 1 && b[5] == 1 && b[8] == 1) { Win(1); }
+			if (b[0] == 1 && b[4] == 1 && b[8] == 1) { Win(1); }
+			if (b[2] == 1 && b[4] == 1 && b[6] == 1) { Win(1); }//end of x win conditions
+
+			if (b[0] == 2 && b[1] == 2 && b[2] == 2) { Win(2); }//Start of o win conditions
+			if (b[3] == 2 && b[4] == 2 && b[5] == 2) { Win(2); }
+			if (b[6] == 2 && b[7] == 2 && b[8] == 2) { Win(2); }
+			if (b[0] == 2 && b[3] == 2 && b[6] == 2) { Win(2); }
+			if (b[1] == 2 && b[4] == 2 && b[7] == 2) { Win(2); }
+			if (b[2] == 2 && b[5] == 2 && b[8] == 2) { Win(2); }
+			if (b[0] == 2 && b[4] == 2 && b[8] == 2) { Win(2); }
+			if (b[2] == 2 && b[4] == 2 && b[6] == 2) { Win(2); }//end of o win conditions
+
+			int catsGameCounter = 0;
+
+			for (int i = 0; i < b.Length; i++)
+			{
+				if(b[i] != 0) { catsGameCounter++; }
+			}
+
+			if(catsGameCounter == 9) { Win(0); }
+		}
+
+		public static void Win(int i)
+		{
+			Console.Clear();
+			if(i == 1)
+			{
+				PrintGameBoard();
+				Console.WriteLine("You won!  Press any key to start again.");
+				Console.ReadKey();
+				ResetGame();
+				iterations = 0;
+				Main(new string[0]);
+			}
+			else if(i == 2)
+			{
+				PrintGameBoard();
+				Console.WriteLine("You lost.  Press any key to start again.");
+				Console.ReadKey();
+				ResetGame();
+				iterations = 0;
+				Main(new string[0]);
+			}else if(i == 0)
+			{
+				PrintGameBoard();
+				Console.WriteLine("Its a cats game!  Press any key to start again");
+				Console.ReadKey();
+				ResetGame();
+				iterations = 0;
+				Main(new string[0]);
+
+
+			}
+		}
+
+		public static void ResetGame()
+		{
+			for (int i = 0; i < gameBoard.Length; i++)
+			{
+				gameBoard[i] = 0;
+			}
+
+			AI.wentFirst = false;
+			AI.moveSuccesful = false;
+
+			AI.move = 0;
+			AI.forkNum = 0;
+			AI.thirdForkNum = 0;
+		}
+    }
+}
